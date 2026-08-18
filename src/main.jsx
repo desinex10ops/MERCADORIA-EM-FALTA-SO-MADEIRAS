b5de0f5133ac784007bf1b06c44d6b007a48cdb4
Marcelo <marcelo270carvalho@gmail.com>
2026-08-18 13:54:13 -0300

    fix: tela de recuperacao automatica caso React nao renderize e limpeza total de Service Workers e caches

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
@@ -6,14 +6,24 @@ import { AuthProvider } from './context/AuthContext.jsx'
 import { DataProvider } from './context/DataContext.jsx'
 import ErrorBoundary from './components/ErrorBoundary.jsx'
 
+function AppWrapper() {
+  useEffect(() => {
+    window.__appRendered = true;
+  }, []);
+
+  return (
+    <AuthProvider>
+      <DataProvider>
+        <App />
+      </DataProvider>
+    </AuthProvider>
+  );
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ErrorBoundary>
-      <AuthProvider>
-        <DataProvider>
-          <App />
-        </DataProvider>
-      </AuthProvider>
+      <AppWrapper />
     </ErrorBoundary>
   </React.StrictMode>,
 )
